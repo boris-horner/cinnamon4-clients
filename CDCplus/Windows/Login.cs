1003565f1bf5683aeab919341aa0300f7a6baacc
boris-horner <boris.horner@texolution.eu>
2024-11-07 17:22:16 +0100

    Login and server entry resizing

--- CDCplus/Windows/Login.cs
+++ CDCplus/Windows/Login.cs
@@ -17,46 +17,51 @@ using System.Windows.Forms;
 
 namespace CDCplus.Windows
 {
-	public partial class Login : Form
-	{
-		private readonly bool _showSaveCredentials;
-		public Login(string serverName, bool showSaveCredentials)
-		{
-			InitializeComponent();
-			_showSaveCredentials = showSaveCredentials;
-			chkSaveCredentials.Visible = _showSaveCredentials;
-			Text = Resources.lblLogin + " " + serverName;
-			cmdCancel.Text = Resources.lblCancel;
-			cmdOk.Text = Resources.lblOk;
-			lblPassword.Text = Resources.lblPassword;
-			lblUser.Text = Resources.lblUsername;
-			cmdOk.Enabled = false;
-		}
+    public partial class Login : Form
+    {
+        private readonly bool _showSaveCredentials;
+        public Login(string serverName, bool showSaveCredentials)
+        {
+            InitializeComponent();
+            _showSaveCredentials = showSaveCredentials;
+            chkSaveCredentials.Visible = _showSaveCredentials;
+            Text = Resources.lblLogin + " " + serverName;
+            cmdCancel.Text = Resources.lblCancel;
+            cmdOk.Text = Resources.lblOk;
+            lblPassword.Text = Resources.lblPassword;
+            lblUser.Text = Resources.lblUsername;
+            cmdOk.Enabled = false;
+        }
 
-		private void cmdOk_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.OK;
-			Close();
-		}
+        private void cmdOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
 
-		private void cmdCancel_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.Cancel;
-			Close();
-		}
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
 
-		public string Username { get { return txtUser.Text; } }
-		public string Password { get { return txtPassword.Text; } }
-		public bool SaveCredentials { get { return chkSaveCredentials.Checked; } }
+        public string Username { get { return txtUser.Text; } }
+        public string Password { get { return txtPassword.Text; } }
+        public bool SaveCredentials { get { return chkSaveCredentials.Checked; } }
 
-		private void txtUser_TextChanged(object sender, EventArgs e)
-		{
-			cmdOk.Enabled = txtUser.Text.Length > 0 && txtPassword.Text.Length > 0;
-		}
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+            cmdOk.Enabled = txtUser.Text.Length > 0 && txtPassword.Text.Length > 0;
+        }
 
-		private void txtPassword_TextChanged(object sender, EventArgs e)
-		{
-			cmdOk.Enabled = txtUser.Text.Length > 0 && txtPassword.Text.Length > 0;
-		}
-	}
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            cmdOk.Enabled = txtUser.Text.Length > 0 && txtPassword.Text.Length > 0;
+        }
+
+        private void Login_Paint(object sender, PaintEventArgs e)
+        {
+            pLogo.Width = pLogo.Height / 3;
+        }
+    }
 }
